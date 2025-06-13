7c83594e1b65563d9d44668fb9fa493e124a73cb
marcoswillianl1987 <marcoswillianl1987@gmail.com>
2025-06-13 14:49:10 +0000

    Corrigi o tamanho dos cards de bebida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
                 Escolha aqui os melhores lanches da cidade
               </CardDescription>
             </CardHeader>
-            <CardContent className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mx-auto">
+            <CardContent className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 ">
               {produtos.filter((produto) => produto.tipo === "lanche").map((produto) => (
                 <ProdutoCard key={produto.id} produto={produto} />
               ))}
@@ -44,14 +44,14 @@ export default function Home() {
           </Card>
         </TabsContent>
         <TabsContent value="bebida">
-          <Card>
+          <Card className="w-full">
             <CardHeader>
               <CardTitle>Bebidas</CardTitle>
               <CardDescription>
                 Escolha aqui as bebidas mais refrescantes da cidade
               </CardDescription>
             </CardHeader>
-            <CardContent className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mx-auto">
+            <CardContent className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 ">
               {produtos.filter((produto) => produto.tipo === 'bebida').map((produto) => (
                 <ProdutoCard key={produto.id} produto={produto} />
               ))}
